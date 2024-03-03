48e455a549b408ff185a73fd964a85979454b295
flaim <likeyt60@gmail.com>
2024-03-03 03:21:26 +0300

    1.0v

---
+++ Author.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+class Author:
+    
+    def __init__(self, token):
+        self.header = {
+            "Authorization": token
+        }
+
+    def user_name(self):
+        response = requests.get(url="https://discord.com/api/v9/users/@me", headers=self.header)
+        data = json.loads(response.text)
+        return data.get("username")
+
+    def id(self):
+        response = requests.get(url="https://discord.com/api/v9/users/@me", headers=self.header)
+        data = json.loads(response.text)
+        return data.get("id")
