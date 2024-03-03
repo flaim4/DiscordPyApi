f87d1758a90ad917eb6fe29d0809251768634dbf
flaim <likeyt60@gmail.com>
2024-03-03 03:43:27 +0300

    1.2v

--- Author.py
+++ Author.py
@@ -4,21 +4,22 @@ import json
 class Author:
     
     def __init__(self, token):
-        self.header = {
+        self._header = {
             "Authorization": token
         }
+        self._user_data = self._fetch_user_data()
+
+
+    def _fetch_user_data(self):
+        response = requests.get(url="https://discord.com/api/v9/users/@me", headers=self._header)
+        data = json.loads(response.text)
+        return data
 
     def user_name(self):
-        response = requests.get(url="https://discord.com/api/v9/users/@me", headers=self.header)
-        data = json.loads(response.text)
-        return data.get("username")
-
+        return self._user_data.get("username")
+    
     def id(self):
-        response = requests.get(url="https://discord.com/api/v9/users/@me", headers=self.header)
-        data = json.loads(response.text)
-        return data.get("id")
+        return self._user_data.get("id")
     
     def global_name(self):
-        response = requests.get(url="https://discord.com/api/v9/users/@me", headers=self.header)
-        data = json.loads(response.text)
-        return data.get("global_name")
+        return self._user_data.get("global_name")
