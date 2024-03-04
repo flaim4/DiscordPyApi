1e88532c09913cf2e1149cd106f60d846201d183
flaim <105062508+flaim4@users.noreply.github.com>
2024-03-04 14:59:43 +0300

    1.5v

--- Message.py
+++ Message.py
@@ -19,3 +19,16 @@ class Message:
         response = requests.post(url, headers=self._header, data=payload)
 
         return response.text
+    
+    def reply_message_channel(self, channel_id , message_id, message):
+        url = f"https://discord.com/api/v9/channels/{channel_id}/messages"
+        payload = json.dumps({
+            "content": message,
+            "message_reference": {
+                "channel_id": channel_id,
+                "message_id": message_id
+            }
+        })
+        response = requests.post(url=url, headers=self._header, data=payload)
+
+        return response.text
