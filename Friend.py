b469d26531276a62640f34ea56ef36e51efa1a08
flaim <105062508+flaim4@users.noreply.github.com>
2024-03-05 23:40:44 +0300

    1.9v

--- Friend.py
+++ Friend.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Friend:
     def __init__(self, token):
@@ -9,5 +10,22 @@ class Friend:
     def get_list_frends(self):
         url = "https://discord.com/api/v9/users/@me/relationships"
         response = requests.get(url=url, headers=self._header)
-
         return response.text
+    
+    def get_list_id_frends(self):
+        url = "https://discord.com/api/v9/users/@me/relationships"
+        response = requests.get(url=url, headers=self._header)
+        data = json.loads(response.text)
+        return [friend["id"] for friend in data]
+    
+    def get_list_global_name_frends(self):
+        url = "https://discord.com/api/v9/users/@me/relationships"
+        response = requests.get(url=url, headers=self._header)
+        data = json.loads(response.text)
+        return [friend["user"]["global_name"] for friend in data]
+    
+    def get_list_username_frends(self):
+        url = "https://discord.com/api/v9/users/@me/relationships"
+        response = requests.get(url=url, headers=self._header)
+        data = json.loads(response.text)
+        return [friend["user"]["username"] for friend in data]
