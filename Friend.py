33560683f16cce6aedf494ab0c43004de8982078
flaim <105062508+flaim4@users.noreply.github.com>
2024-03-05 23:02:01 +0300

    1.8v

--- Friend.py
+++ Friend.py
@@ -1,3 +1,13 @@
-class Friends:
-    def __init__(self):
-        pass
+import requests
+
+class Friend:
+    def __init__(self, token):
+        self._header = {
+            "Authorization": token
+        }
+
+    def get_list_frends(self):
+        url = "https://discord.com/api/v9/users/@me/relationships"
+        response = requests.get(url=url, headers=self._header)
+
+        return response.text
--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .Author import Author
 from .User import User
 from .Message import Message
 from .Friend import Friend
-from .Guild import Message
+from .Guild import Guild
