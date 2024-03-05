0ee44d25ae2691404cd068683e9cb86ee5aff7ba
flaim <105062508+flaim4@users.noreply.github.com>
2024-03-05 23:47:22 +0300

    1.10v

--- Message.py
+++ Message.py
@@ -44,3 +44,17 @@ class Message:
         respose = requests.get(url=url, headers=self._header)
         
         return respose.text
+    
+    def get_message_id_channel(self, channel_id, limit=100):
+        url = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}"
+        respose = requests.get(url=url, headers=self._header)
+        data = json.loads(respose.text)
+        
+        return [message_id["id"] for message_id in data]
+    
+    def get_message_content_channel(self, channel_id, limit=100):
+        url = f"https://discord.com/api/v9/channels/{channel_id}/messages?limit={limit}"
+        respose = requests.get(url=url, headers=self._header)
+        data = json.loads(respose.text)
+        
+        return [message_id["content"] for message_id in data]
