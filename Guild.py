123b2c9fabe953468b1bcae05e552f1216602000
flaim <105062508+flaim4@users.noreply.github.com>
2024-03-07 00:06:49 +0300

    1.12v

--- Guild.py
+++ Guild.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class Guild:
     def __init__(self, token):
@@ -10,3 +11,21 @@ class Guild:
         url="https://discordapp.com/api/users/@me/guilds"
         response = requests.get(url=url, headers=self._header)
         return response.text
+
+    def get_id_guilds(self):
+        url="https://discordapp.com/api/users/@me/guilds"
+        response = requests.get(url=url, headers=self._header)
+        data = json.loads(response.text)
+        return [guild["id"] for guild in data]
+    
+    def get_name_guilds(self):
+        url="https://discordapp.com/api/users/@me/guilds"
+        response = requests.get(url=url, headers=self._header)
+        data = json.loads(response.text)
+        return [guild["name"] for guild in data]
+    
+    def get_channel_guilds(self, guild_id):
+        url=f"https://discord.com/api/v9/guilds/{guild_id}/channels"
+        response = requests.get(url=url, headers=self._header)
+        data = json.loads(response.text)
+        return [guild["name"] for guild in data]
