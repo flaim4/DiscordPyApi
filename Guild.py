c5ffdda32e5f2aa9cea77acfe9a7cc0b6c72923a
flaim <105062508+flaim4@users.noreply.github.com>
2024-03-06 22:13:58 +0300

    1.11v

--- Guild.py
+++ Guild.py
@@ -1,3 +1,12 @@
+import requests
+
 class Guild:
-    def __init__(self):
-        pass
+    def __init__(self, token):
+        self._header = {
+            "Authorization": token
+        }
+
+    def get_guilds(self):
+        url="https://discordapp.com/api/users/@me/guilds"
+        response = requests.get(url=url, headers=self._header)
+        return response.text
